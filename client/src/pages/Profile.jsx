c8ea8f200d1630fac8ae40b95699304c238de36c
Kibe Kevin <kibekevin215@gmail.com>
2025-05-06 17:29:30 +0300

    Complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
+import UploadWidget from '../components/uploadWidget/uploadWidget.jsx';
 
 const Profile = () => {
 
     const { currentUser } = useSelector((state) => state.user);
+    const [avatar, setAvatar] = useState(currentUser.avatar);
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
         <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+            
             <form className='flex flex-col gap-4'>
-                <img className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-1' src={currentUser.avatar} alt="avatar" />
+                <img className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-1' src={avatar} alt="avatar" />
+                
+                <UploadWidget uwConfig={{
+                    cloudName : "dnfnbnmwr",
+                    uploadPreset: "peaknest",
+                    multiple: false,
+                    maxImageFileSize: 3000000,
+                    folder: "avatars"
+                    }}
+                    setAvatar={setAvatar}
+                />
+
                 <input type="username" placeholder='username' id='username' className='border p-3 rounded-lg' />
                 <input type="email" placeholder='email' id='email' className='border p-3 rounded-lg' />
                 <input type="password" placeholder='password' id='password' className='border p-3 rounded-lg' />
