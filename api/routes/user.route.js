e7eb9840285ea032eb295219e89be2fae7aeb605
Kibe Kevin <kibekevin215@gmail.com>
2025-05-19 23:34:30 +0300

    Refactor user.route.js: reorder route definitions for consistency and clarity while maintaining functionality.

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,13 +3,12 @@ import { deleteUser, test, getUserListings, getUser } from '../controllers/user.
 import { updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
-
 const router = express.Router();
 
 router.get('/test', test);
-router.post('/update/:id',verifyToken, updateUser);
-router.delete('/delete/:id',verifyToken, deleteUser);
-router.get('/listings/:id',verifyToken, getUserListings);
+router.get('/listings/:id', verifyToken, getUserListings);
+router.post('/update/:id', verifyToken, updateUser);
+router.delete('/delete/:id', verifyToken, deleteUser);
 router.get('/:id', verifyToken, getUser);
 
 export default router; 
